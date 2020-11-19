a93aea4f7566c1395c2344fe9c0c5890f4fc7aea
GitSeob <anhs0220@gmail.com>
2020-11-19 16:13:26 +0900

    mouse 위치에 따른 rect 그리기 성공

--- front/layouts/Board/index.tsx
+++ front/layouts/Board/index.tsx
@@ -1,37 +1,67 @@
-import React, {FC, useEffect} from 'react';
+import React, {FC} from 'react';
 import useSWR from 'swr';
 import fetcher from '@utils/fetcher';
 import LoadingCircle from '@components/LoadingCircle';
 import { Redirect } from 'react-router-dom';
 
 import {Stage, Layer, Rect} from 'react-konva';
+import Konva from 'konva';
+
+type Position = {
+    x: number,
+    y: number
+};
 
 const WorkSpace:FC = () => {
-    const height = 1200;
-    const width = 1920;
+    const layerRef = React.useRef() as React.MutableRefObject<Konva.Layer>;
+    const [mPos, setMPost] = React.useState<Position>({
+        x: 0,
+        y: 0,
+    })
+    const [rPos, setRPos] = React.useState<Position>({
+        x: 0,
+        y: 0
+    })
+    const height = window.innerHeight;
+    const width = height * 16 / 10;
 
-    const rectSize = 60;
+    const rectSize = width / 20;
 
-    const RectOnCanvas = ({x = 60, y = 60}) => {
-        console.log(x, y);
-        return <Rect width={rectSize} height={rectSize} fill='#89b717' x={x} y={y}/>
+    const RectOnCanvas = ({x = rectSize, y = rectSize}) => {
+        return <Rect
+            width={rectSize}
+            height={rectSize}
+            fill='rgba(255, 255, 255, 0.1)'
+            x={x}
+            y={y}
+            cornerRadius={5}
+            />
     }
-    const xarr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-    const yarr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
-    console.log({xarr, yarr});
+    React.useEffect(() => {
+        setRPos({
+            x: mPos.x - mPos.x % rectSize,
+            y: mPos.y - mPos.y % rectSize
+        })
+    }, [mPos]);
 
     return (
         <>
-            <Stage width={width} height={height}>
-                <Layer>
-                    <RectOnCanvas/>
-                    {xarr.map((x, i) => {
-                        yarr.map((y, j) => {
-                            console.log(x, y)
-                            return <RectOnCanvas key={(100 * x + y)} x={360 + 60*x} y={360 + 60*y} />
-                        })
-                    })}
+            <Stage
+                width={width}
+                height={height}
+                onMouseMove={(e) => {
+                    const transform = layerRef.current.getAbsoluteTransform().copy();
+                    transform.invert();
+                    const pos = e.target.getStage()?.getPointerPosition();
+                    setMPost({
+                        x: pos?.x as number,
+                        y: pos?.y as number,
+                    })
+                }}
+            >
+                <Layer ref={layerRef}>
+                    <RectOnCanvas x={rPos.x} y={rPos.y}/>
                 </Layer>
             </Stage>
         </>
