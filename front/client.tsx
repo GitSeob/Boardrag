ddfd8177119541a70ccbbb4b9d82643a6ad754c1
GitSeob <anhs0220@gmail.com>
2020-12-03 17:25:48 +0900

    test build - gif CORS Error

--- front/client.tsx
+++ front/client.tsx
@@ -8,6 +8,7 @@ import App from './layouts/App';
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? 'https://api.42board.com' : 'http://localhost:3095';
 axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers['Access-Control-Allow-Credentials'] = true;
 
 render(
     <BrowserRouter>
