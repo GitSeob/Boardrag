a7e5bdf47f11efa54b9c54ec6025bd64a4a74f92
GitSeob <anhs0220@gmail.com>
2020-12-10 16:07:56 +0900

    function - chat data to section structrue

---
+++ front/utils/createSection.ts
@@ -0,0 +1,22 @@
+import moment from 'moment';
+
+interface Props {
+    id: number,
+    userId: number,
+    username: string,
+    content: string,
+    createdAt: Date,
+}
+
+export default function makeSection<T extends Props>(chatList: T[]) {
+    const sections: { [key: string]: T[] } = {};
+    chatList.forEach((chat) => {
+        const monthDate = moment(chat.createdAt).format('YYYY-MM-DD');
+        if (Array.isArray(sections[monthDate])) {
+            sections[monthDate].push(chat);
+        } else {
+            sections[monthDate] = [chat];
+        }
+    });
+    return sections;
+}
