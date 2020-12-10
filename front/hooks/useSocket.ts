9173b64791a2948df57b551de2576ac9532fed20
GitSeob <anhs0220@gmail.com>
2020-12-10 16:11:28 +0900

    edit socket name

--- front/hooks/useSocket.ts
+++ front/hooks/useSocket.ts
@@ -4,9 +4,9 @@ import io from 'socket.io-client';
 
 const backUrl = process.env.NODE_ENV === 'production' ? 'https://api.42board.com' : 'http://localhost:3095';
 
-const sockets: { [key: number]: SocketIOClient.Socket } = {};
+const sockets: { [key: string]: SocketIOClient.Socket } = {};
 
-const useSocket = (board?: number): [SocketIOClient.Socket | undefined, () => void] => {
+const useSocket = (board?: string): [SocketIOClient.Socket | undefined, () => void] => {
     const disconnect = useCallback(() => {
         if (board && sockets[board]) {
             sockets[board].disconnect();
@@ -19,7 +19,7 @@ const useSocket = (board?: number): [SocketIOClient.Socket | undefined, () => vo
     }
 
     if (!sockets[board]) {
-        sockets[board] = io(`${backUrl}/ws-${board}`, {
+        sockets[board] = io(`${backUrl}/board-${board}`, {
             transports: ['websocket'],
         });
         console.info('create socket', board, sockets[board].id);
