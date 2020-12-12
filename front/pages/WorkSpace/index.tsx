a6f92067ba5187a8a18babbeeeee1805c3b407a3
GitSeob <anhs0220@gmail.com>
2020-12-12 19:32:12 +0900

    v0.0.5 - 만료일 주석으로 처리

--- front/pages/WorkSpace/index.tsx
+++ front/pages/WorkSpace/index.tsx
@@ -902,9 +902,7 @@ const WorkSpace:FC<IBoardProps> = ({ board, boardData, dataReval, userData }) =>
                         </UserInfo>
                         <MomentBox>
                             <div>작성일 : <p>{dayjs(openDetail.content?.createdAt).format('YYYY년 MM월 DD일')}</p></div>
-                            <div>만료일 : <p>{dayjs(openDetail.content?.expiry_date).format('YYYY년 MM월 DD일')}</p></div>
-                            {/* <div>작성일 : <p>{moment(openDetail.content?.createdAt).format('YYYY년 MM월 DD일')}</p></div>
-                            <div>만료일 : <p>{moment(openDetail.content?.expiry_date).format('YYYY년 MM월 DD일')}</p></div> */}
+                            {/* <div>만료일 : <p>{dayjs(openDetail.content?.expiry_date).format('YYYY년 MM월 DD일')}</p></div> */}
                         </MomentBox>
                         Content :
                         <DetailContentBox>
@@ -1030,7 +1028,6 @@ const WorkSpace:FC<IBoardProps> = ({ board, boardData, dataReval, userData }) =>
                                         <p>{c.User.username}</p>
                                         <div>
                                             <div>{c.content}</div>
-                                            {/* <p>{moment(c.createdAt).diff(now, 'days') > -1 ? moment(c.createdAt).format('LT') : moment(c.createdAt).format('YYYY년 MM월 DD일')}</p> */}
                                             <p>{dayjs(c.createdAt).diff(now, 'day') > -1 ? dayjs(c.createdAt).format('LT') : dayjs(c.createdAt).format('YYYY년 MM월 DD일')}</p>
                                         </div>
                                     </div>
