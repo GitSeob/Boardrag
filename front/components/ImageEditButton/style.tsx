8536dde6adc5ce19287f4713ad07fc19d196f362
GitSeob <anhs0220@gmail.com>
2020-12-13 20:30:09 +0900

    ImageEditButton 컴포넌트 분리

---
+++ front/components/ImageEditButton/style.tsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+export const EditImageInput = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 50%;
+    padding: .5rem 1rem;
+    background: rgba(0, 0, 0, 0.4);
+    color: #fff;
+    border-radius: 3px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 2rem;
+    cursor: pointer;
+
+    & > svg {
+        fill: #fff;
+        height: 2rem;
+    }
+`;
--- front/pages/WorkSpace/style.tsx
+++ front/pages/WorkSpace/style.tsx
@@ -667,26 +667,3 @@ export const BoardFooter = styled.div`
     height: 100%;
     background: rgba(0, 0, 0, .1);
 `;
-
-
-export const EditImageInput = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 50%;
-    padding: .5rem 1rem;
-    background: rgba(0, 0, 0, 0.4);
-    color: #fff;
-    border-radius: 3px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 2rem;
-    cursor: pointer;
-
-    & > svg {
-        fill: #fff;
-        height: 2rem;
-    }
-`;
