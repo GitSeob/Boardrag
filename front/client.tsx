02b04a5e125807fa9334a4b2f9afc2393c9bbc10
GitSeob <anhs0220@gmail.com>
2021-03-19 23:19:54 +0900

    css 초기화 및 초기설정 추가

--- front/client.tsx
+++ front/client.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
-
 import App from './layouts/App';
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? 'https://api.42board.com' : 'http://localhost:3095';
-axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers['Access-Control-Allow-Origin'] = '*'; // CORS 해결 방법 (보안에 취약하긴 하다.)
 
 render(
 	<BrowserRouter>
